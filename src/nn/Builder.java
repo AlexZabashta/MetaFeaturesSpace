98e369e26675d2dfee20aa26cf6b6225e3b112d5
Alexey Zabashta <AlexZabashta@github.com>
2018-03-30 18:30:04 +0300

    add depth in cnn layers

--- src/nn/Builder.java
+++ src/nn/Builder.java
@@ -4,8 +4,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 import nn.act.Tanh;
+import nn.fld.Max;
 import nn.fld.One;
 import nn.fld.Sum;
+import weka.core.logging.OutputLogger.OutputPrintStream;
 
 public class Builder {
     public static NeuralNetwork full(int... layerSizes) {
@@ -75,70 +77,90 @@ public class Builder {
         return new NeuralNetwork(inpSize, outSize, numWeights, neurons);
     }
 
-    public static NeuralNetwork cnnSharedLayer(int inpW, int inpH, int winW, int winH, Activation activation) {
+    public static NeuralNetwork cnnSharedLayer(int inpW, int inpH, int inpD, int winW, int winH, int outD, Activation activation) {
 
         int outW = inpW - winW + 1;
         int outH = inpH - winH + 1;
 
-        int inpSize = inpW * inpH;
-        int outSize = outW * outH;
+        int inpSize = inpW * inpH * inpD;
+        int outSize = outW * outH * outD;
 
         int numWeights = 0;
         Neuron[] neurons = new Neuron[outSize];
 
-        int dendrites = winW * winH;
-        int[] wid = new int[dendrites];
-        for (int d = 0; d < dendrites; d++) {
-            wid[d] = numWeights++;
+        int dendrites = winW * winH * inpD;
+
+        int[][] wid = new int[outD][dendrites];
+
+        for (int z = 0; z < outD; z++) {
+            for (int d = 0; d < dendrites; d++) {
+                wid[z][d] = numWeights++;
+            }
         }
 
         for (int x = 0, i = 0; x < outW; x++) {
-            for (int y = 0; y < outH; y++, i++) {
+            for (int y = 0; y < outH; y++) {
 
                 int[] from = new int[dendrites];
 
                 for (int dx = 0, j = 0; dx < winW; dx++) {
-                    for (int dy = 0; dy < winH; dy++, j++) {
-                        from[j] = (x + dx) * inpH + (y + dy);
+                    int fx = x + dx;
+                    for (int dy = 0; dy < winH; dy++) {
+                        int fy = y + dy;
+                        for (int fz = 0; fz < inpD; fz++, j++) {
+                            from[j] = ((fx) * inpH + fy) * inpD + fz;
+                        }
                     }
                 }
 
-                neurons[i] = new Sum(dendrites, from, wid, inpSize + i, activation);
+                for (int z = 0; z < outD; z++, i++) {
+                    neurons[i] = new Sum(dendrites, from, wid[z], inpSize + i, activation);
+                }
             }
         }
 
         return new NeuralNetwork(inpSize, outSize, numWeights, neurons);
     }
 
-    public static NeuralNetwork maxPoolLayer(int inpW, int inpH, int scaleW, int scaleH, Activation activation) {
+    public static NeuralNetwork maxPoolLayer(int inpW, int inpH, int inpD, int scaleW, int scaleH, int outD, Activation activation) {
 
         int outW = inpW / scaleW;
         int outH = inpH / scaleH;
 
-        int inpSize = inpW * inpH;
-        int outSize = outW * outH;
+        int inpSize = inpW * inpH * inpD;
+        int outSize = outW * outH * outD;
 
         int numWeights = 0;
         Neuron[] neurons = new Neuron[outSize];
 
-        int dendrites = scaleW * scaleH;
-        int[] wid = new int[dendrites];
-        for (int d = 0; d < dendrites; d++) {
-            wid[d] = numWeights++;
+        int dendrites = scaleW * scaleH * inpD;
+
+        int[][] wid = new int[outD][dendrites];
+
+        for (int z = 0; z < outD; z++) {
+            for (int d = 0; d < dendrites; d++) {
+                wid[z][d] = numWeights++;
+            }
         }
 
         for (int x = 0, i = 0; x < outW; x++) {
-            for (int y = 0; y < outH; y++, i++) {
+            for (int y = 0; y < outH; y++) {
 
                 int[] from = new int[dendrites];
 
                 for (int dx = 0, j = 0; dx < scaleW; dx++) {
-                    for (int dy = 0; dy < scaleH; dy++, j++) {
-                        from[j] = (x * scaleW + dx) * inpH + (y * scaleH + dy);
+                    int fx = x * scaleW + dx;
+                    for (int dy = 0; dy < scaleH; dy++) {
+                        int fy = y * scaleH + dy;
+                        for (int fz = 0; fz < inpD; fz++, j++) {
+                            from[j] = ((fx) * inpH + fy) * inpD + fz;
+                        }
                     }
                 }
 
-                neurons[i] = new Sum(dendrites, from, wid, inpSize + i, activation);
+                for (int z = 0; z < outD; z++, i++) {
+                    neurons[i] = new Max(dendrites, from, wid[z], inpSize + i, activation);
+                }
             }
         }
 
@@ -146,7 +168,47 @@ public class Builder {
     }
 
     public static void main(String[] args) {
-        NeuralNetwork network = full(7, 1, 3);
-        System.out.println(network.size);
+        {
+            NeuralNetwork network = cnnSharedLayer(256, 64, 1, 17, 17, 2, new Tanh());
+            System.out.println(network.inpSize + " " + network.outSize);
+            System.out.println(network.size);
+            System.out.println(network.numWeights);
+        }
+
+        {
+            NeuralNetwork network = maxPoolLayer(240, 48, 2, 4, 2, 3, new Tanh());
+            System.out.println(network.inpSize + " " + network.outSize);
+            System.out.println(network.size);
+            System.out.println(network.numWeights);
+        }
+
+        {
+            NeuralNetwork network = cnnSharedLayer(60, 24, 3, 5, 5, 4, new Tanh());
+            System.out.println(network.inpSize + " " + network.outSize);
+            System.out.println(network.size);
+            System.out.println(network.numWeights);
+        }
+
+        {
+            NeuralNetwork network = maxPoolLayer(56, 20, 4, 4, 2, 5, new Tanh());
+            System.out.println(network.inpSize + " " + network.outSize);
+            System.out.println(network.size);
+            System.out.println(network.numWeights);
+        }
+
+        {
+            NeuralNetwork network = cnnSharedLayer(14, 10, 5, 7, 3, 6, new Tanh());
+            System.out.println(network.inpSize + " " + network.outSize);
+            System.out.println(network.size);
+            System.out.println(network.numWeights);
+        }
+
+        {
+            NeuralNetwork network = maxPoolLayer(8, 8, 6, 4, 2, 5, new Tanh());
+            System.out.println(network.inpSize + " " + network.outSize);
+            System.out.println(network.size);
+            System.out.println(network.numWeights);
+        }
+
     }
 }
